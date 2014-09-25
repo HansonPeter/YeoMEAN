8630eb18fe5bf04f80161ed7de6a6eb1b6cfee18
Jacoby04 <opdah023@morris.umn.edu>
2014-09-25 13:31:01 -0500

    Added tests, but unable to run them, and added some comments

--- client/app/gpacalc/gpacalc.controller.js
+++ client/app/gpacalc/gpacalc.controller.js
@@ -6,75 +6,79 @@
  */
 
 angular.module('yeoMeanApp')
-  .controller('GpacalcCtrl', function ($scope, $http) {
+    .controller('GpacalcCtrl', function ($scope, $http) {
 
-        // Each class corresponds to a row in the table for our calculator
-        // and is updated using data-binding.
-        // This fills up by getting courses from the database in various places.
-        $scope.classes = [];
+    // Each class corresponds to a row in the table for our calculator
+    // and is updated using data-binding.
+    // This fills up by getting courses from the database in various places.
+    $scope.classes = [];
 
-        // Updates calculator with courses from the database anytime the page loads.
-        $http.get('/api/courses').success(function(classes) {
-            $scope.classes = classes;
-        });
+    // Updates calculator with courses from the database anytime the page loads.
+    $http.get('/api/courses').success(function(classes) {
+        $scope.classes = classes;
+    });
 
-        /*
-         * Originally used to make drop-down buttons, kept later as reference
-         * to know the number of possible credits desired.
-        $scope.credits = [
-            0,
-            1,
-            2,
-            3,
-            4,
-            5
-        ];
-        */
+    /*
+     * Originally used to make drop-down buttons, kept later as reference
+     * to know the number of possible credits desired.
+     $scope.credits = [
+     0,
+     1,
+     2,
+     3,
+     4,
+     5
+     ];
+     */
 
-            $scope.convertGradeToGPA = function(grade) { //This is a function that is a switch to determine how many
-                switch (grade) { //grade points to give for a grade. If any other letter is given
-                    case "A": //that is not a traditional grade, the default amount of grade points
-                        return 4; //given is zero.
-                        break;
-                    case "A-":
-                        return 3.7;
-                        break;
-                    case "B+":
-                        return 3.33;
-                        break;
-                    case "B":
-                        return 3;
-                        break;
-                    case "B-":
-                        return 2.7;
-                        break;
-                    case "C+":
-                        return 2.3;
-                        break;
-                    case "C":
-                        return 2;
-                        break;
-                    case "C-":
-                        return 1.7;
-                        break;
-                    case "D+":
-                        return 1.3;
-                        break;
-                    case "D":
-                        return 1;
-                        break;
-                    case "F":
-                        return 0;
-                        break;
-                    default:
-                        return 0;
-                };
+     /*
+     This is a function that is a switch to determine how many grade points to give for a grade. If any letter
+     other than these are given, the default amount of grade points given is zero.
+      */
+     $scope.convertGradeToGPA = function(grade) {
+            switch (grade) {
+                case "A":
+                    return 4;
+                    break;
+                case "A-":
+                    return 3.7;
+                    break;
+                case "B+":
+                    return 3.33;
+                    break;
+                case "B":
+                    return 3;
+                    break;
+                case "B-":
+                    return 2.7;
+                    break;
+                case "C+":
+                    return 2.3;
+                    break;
+                case "C":
+                    return 2;
+                    break;
+                case "C-":
+                    return 1.7;
+                    break;
+                case "D+":
+                    return 1.3;
+                    break;
+                case "D":
+                    return 1;
+                    break;
+                case "F":
+                    return 0;
+                    break;
+                default:
+                    return 0;
             };
+     };
 
-        // Uses the information in the classes array which has been updated
-        // using data binding to calculate the GPA and instantly display it
-        // using even more data-binding.
-        $scope.calculateGPA = function() {
+     // Uses the information in the classes array which has been updated
+     // using data binding to calculate the GPA and instantly display it
+     // using even more data-binding.
+     $scope.calculateGPA = function() {
             var GPA = 0;
             var totalCredits = 0;
             for (var i = 0; i < $scope.classes.length; ++i) {
@@ -85,11 +89,12 @@ angular.module('yeoMeanApp')
                 return "Your estimated GPA: " + (GPA / totalCredits);
             } else {
                 return "You need to select courses and credits!";
-            }
+            }                   //Update classes to have the same data that's in the database on the server
+
         };
 
-        // Adds another class to be used to calculate the GPA.
-        $scope.addClass = function() {
+     // Adds another class to be used to the database, and then updates the array.
+     $scope.addClass = function() {
             if ($scope.courseName === '') {
                 return;
             }
@@ -97,7 +102,7 @@ angular.module('yeoMeanApp')
                                         grade: $scope.gradeValue,
                                         gradePoints: $scope.convertGradeToGPA($scope.gradeValue),
                                         credits: $scope.creditValue}).success(function(){
-                //Update classes to have same data that's in the database on the server
+                //Update class to have same data that's in the database on the server
                 $http.get('/api/courses').success(function(classes) {
                     $scope.classes = classes;
                 });
@@ -105,16 +110,16 @@ angular.module('yeoMeanApp')
                 $scope.gradeValue = '';
                 $scope.creditValue = '';
             });
-        };
+     };
 
-        // Removes a class being used to calculate the GPA from the database.
-        // Then updates the array based on the db.
-        $scope.removeClass = function(course) {
-            $http.delete('/api/courses/' + course._id).success(function(){
-                //Update classes to have the same data that's in the database on the server
-                $http.get('/api/courses').success(function(classes) {
-                    $scope.classes = classes;
-                });
-            });
-        };
+           // Removes a class being used to calculate the GPA from the database.
+           // Then updates the array based on the db.
+           $scope.removeClass = function(course) {
+               $http.delete('/api/courses/' + course._id).success(function(){
+                   //Update classes to have the same data that's in the database on the server
+                   $http.get('/api/courses').success(function(classes) {
+                       $scope.classes = classes;
+                   });
+               });
+           };
   });
--- client/app/gpacalc/gpacalc.controller.spec.js
+++ client/app/gpacalc/gpacalc.controller.spec.js
@@ -1,21 +1,70 @@
 'use strict';
 
+
+ /*
+ Jacob Opdahl, Peter Hanson, Emma Sax
+ Lab 5
+ */
+
 describe('Controller: GpacalcCtrl', function () {
 
   // load the controller's module
   beforeEach(module('yeoMeanApp'));
 
-  var GpacalcCtrl, scope;
+  var GpacalcCtrl, scope, httpBackend, classes;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    GpacalcCtrl = $controller('GpacalcCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+      scope = $rootScope.$new();
+      httpBackend = $httpBackend;
+      GpacalcCtrl = $controller('GpacalcCtrl', {
+          $scope: scope
+      });
+
+      classes = [];
+
+      //The following httpBackend things will determine what will happen when an http request is called during our tests
+      httpBackend.when('POST', '/api/courses').respond(function(method, url, data, headers) {
+          classes.push(JSON.parse(data));
+          return [200, {}, {}];
+      });
+
+      httpBackend.when('GET', '/api/courses').respond(classes);
+
+      //The 1 is for identifying the _id on the movie in the "database"
+      httpBackend.when('DELETE', '/api/courses/1').respond(function() {
+          classes.splice(classes.indexOf({name:"IS 2001H", grade: "A", gradePoints: 4, credits: 2}),1);
+          return [200, {}, {}];
+      });
   }));
 
   it('should ...', function () {
-    expect(1).toEqual(1);
+      expect(1).toEqual(1);
   });
-});
+
+    //We're not exactly sure how to run the tests....
+    describe('testing add and delete from the database', function() {
+        it('should add a new course "IS 2001H" with a grade of "A", gradePoints of 4, and credits of 2 and return it', function() {
+            scope.courseName = "IS 2001H";
+            scope.gradeValue = "A";
+            scope.creditValue = 2;
+            scope.addClass();
+            //all of the http requests are added into a queue and are all run when the flush() method is called
+            httpBackend.flush();
+            expect(classes[0]).toEqual({name:"IS 2001H", grade: "A", gradePoints: 4, credits: 2});
+        });
+
+        it('should return empty object', function() {
+            scope.courseName = "IS 2001H";
+            scope.gradeValue = "A";
+            scope.creditValue = 2;
+            scope.addClass();
+            //The _id is used so the correct course is deleted from the database
+            scope.removeClass({name:"IS 2001H", grade: "A", gradePoints: 4, credits: 2, _id:1});
+            httpBackend.flush();
+            expect(classes.length).toEqual(0);
+            expect(classes[0]).toEqual(undefined);
+        });
+
+    });
+})
